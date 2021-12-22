3e71afd6d574de0f7772cbe7629ed038516fd001
moonshadow <1815265375@qq.com>
2021-12-22 12:15:29 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" class="brand-icon">
+    <a href="#" class="footer-link">
+        刻意练习，每日精进。
+    </a>
+    <div class="footer-about">
+        <a href="/about" class="footer-link">关于</a>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="#" class="navbar-brand">Weibo</a>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="#" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登入</a></li>
+        </ul>
+    </div>
+</nav>
