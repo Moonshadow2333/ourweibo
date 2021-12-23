20275f41d9f51775e65bc15a2b82033e2be2970f
moonshadow <1815265375@qq.com>
2021-12-23 14:01:18 +0000

    用户退出登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,23 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a href="{{route('home')}}" class="navbar-brand">Weibo</a>
-        <ul class="navbar-nav">
+        <ul class="navbar-nav justify-content-end">
+            @if(Auth::check())
+            <li class="nav-item"><a href="#" class="nav-link">用户列表</a></li>
+            <li class="nav-item"><a href="{{route('users.show',Auth::user())}}" class="nav-link">个人中心</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">编辑资料</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">
+                <form action="{{route('logout')}}" method="post">
+                    {{csrf_field()}}
+                    {{method_field('DELETE')}}
+                    <button type="submit" class="btn btn-block btn-danger" name="button">退出</button>
+                </form>
+            </a></li>
+            @else
             <li class="nav-item"><a href="{{route('help')}}" class="nav-link">帮助</a></li>
             <li class="nav-item"><a href="#" class="nav-link">登入</a></li>
+
+            @endif
         </ul>
     </div>
 </nav>
