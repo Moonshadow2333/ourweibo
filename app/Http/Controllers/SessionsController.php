fab9eae9c52d08016056a43cbbf4072979ffcf5e
moonshadow <1815265375@qq.com>
2021-12-23 14:18:22 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,7 +14,7 @@ class SessionsController extends Controller
             'email'=>'required|email|max:255',
             'password'=>'required'
         ]);
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('remember'))){
             // 用户身份验证通过。
             session()->flash('success','欢迎回来！');
             return redirect()->route('users.show',[Auth::User()]);
