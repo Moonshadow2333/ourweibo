9f4748bbbe4cfb09d9d46862a14de07c4de3988b
moonshadow <1815265375@qq.com>
2021-12-26 03:08:23 +0000

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('password/email','PasswordController@sendResetLinkEmail')->name('pas
 
 Route::get('password/reset/{token}','PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset','PasswordController@reset')->name('password.update');
+
+// 微博
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
