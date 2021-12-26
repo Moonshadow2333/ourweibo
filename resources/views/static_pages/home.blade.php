499866fa2078060b25270c72138e8236a9aceb10
moonshadow <1815265375@qq.com>
2021-12-26 03:55:00 +0000

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,9 @@
       <div class="col-md-8">
         <section class="status_form">
           @include('shared._status_form')
+          <h4>微博列表</h4>
+          <hr>
+          @include('shared._feed')
         </section>
       </div>
       <aside class="col-md-4">
